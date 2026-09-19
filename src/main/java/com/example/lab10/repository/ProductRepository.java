1f33dc697675a66ff0f0795405831d3e9716dbd8
Suchada <pc231248@gmail.com>
2026-09-19 15:43:58 +0700

    Update Repository

--- src/main/java/com/example/lab10/repository/ProductRepository.java
+++ src/main/java/com/example/lab10/repository/ProductRepository.java
@@ -47,7 +47,11 @@ public class ProductRepository {
      */
     public Mono<Product> findById(String id) {
         // TODO: เติม code ตรงนี้
-        return null; // ← แก้บรรทัดนี้
+        Product p  = store.get(id);
+        if(p==null){
+            return Mono.empty();
+        }
+        return Mono.just(p); // ← แก้บรรทัดนี้
     }
 
     // ── 2. หา Product ทั้งหมด ────────────────────────────
@@ -59,7 +63,7 @@ public class ProductRepository {
      */
     public Flux<Product> findAll() {
         // TODO: เติม code ตรงนี้
-        return null; // ← แก้บรรทัดนี้
+        return Flux.fromIterable(store.values());// ← แก้บรรทัดนี้
     }
 
     // ── 3. บันทึก Product ────────────────────────────────
@@ -71,7 +75,8 @@ public class ProductRepository {
      */
     public Mono<Product> save(Product product) {
         // TODO: เติม code ตรงนี้
-        return null; // ← แก้บรรทัดนี้
+        store.put(product.getId(),product);
+        return Mono.just(product);// ← แก้บรรทัดนี้
     }
 
     // ── 4. ลบ Product ────────────────────────────────────
@@ -83,7 +88,8 @@ public class ProductRepository {
      */
     public Mono<Void> deleteById(String id) {
         // TODO: เติม code ตรงนี้
-        return null; // ← แก้บรรทัดนี้
+        store.remove(id);
+        return Mono.empty(); // ← แก้บรรทัดนี้
     }
 
     // ── 5. กรองตาม category ──────────────────────────────
@@ -95,6 +101,6 @@ public class ProductRepository {
      */
     public Flux<Product> findByCategory(String category) {
         // TODO: เติม code ตรงนี้
-        return null; // ← แก้บรรทัดนี้
+        return findAll().filter(p->p.getCategory().equalsIgnoreCase(category)); // ← แก้บรรทัดนี้
     }
 }
